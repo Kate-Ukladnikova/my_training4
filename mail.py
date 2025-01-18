52c055d46437817134347b3ef8e7f523fc94965f
Kate-Ukladnikova <ekaterinaukladnikova2@gmail.com>
2025-01-18 17:07:33 +0300

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,32 @@
+from aiogram import Dispatcher, executor, types, Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api =""
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+# @dp.message_handler()
+# async def start(message):
+#     print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def urban_message(message):
+    print('Urban message')
+
+# @dp.message_handler(commands=['start'])
+# async def all_message(message):
+#     print('Введите команду /start, чтобы начать общение.')
+
+@dp.message_handler(commands=['start'])
+async def all_message(message):
+    print('Start message')
+
+@dp.message_handler()
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
