75c45f1ea0616d46120b5be6f7fa363cd1f9948f
Kate-Ukladnikova <ekaterinaukladnikova2@gmail.com>
2025-01-19 00:46:07 +0300

    Add files via upload

--- mail.py
+++ mail.py
@@ -1,3 +1,6 @@
+# Цель: написать простейшего телеграм-бота, используя асинхронные функции
+# Задача "Бот поддержки (Начало)"
+
 from aiogram import Dispatcher, executor, types, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
@@ -6,26 +9,13 @@ api =""
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-# @dp.message_handler()
-# async def start(message):
-#     print('Привет! Я бот помогающий твоему здоровью.')
-
-@dp.message_handler(text=['Urban', 'ff'])
-async def urban_message(message):
-    print('Urban message')
-
-# @dp.message_handler(commands=['start'])
-# async def all_message(message):
-#     print('Введите команду /start, чтобы начать общение.')
-
 @dp.message_handler(commands=['start'])
-async def all_message(message):
-    print('Start message')
-
-@dp.message_handler()
 async def start(message):
     print('Привет! Я бот помогающий твоему здоровью.')
 
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
