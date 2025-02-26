c3a4adf09a011474dcf8cd4c30ccd23ff633945c
Kate-Ukladnikova <ekaterinaukladnikova2@gmail.com>
2025-02-26 22:54:07 +0300

    Add files via upload

---
+++ modul_15_4.py
@@ -0,0 +1,2 @@
+# ссылка на Введение в GoogleColab.ipynb с домашним заданием по теме "Введение в GoogleColab":
+# https://colab.research.google.com/drive/1eDW7V_nj-BJsVQNi7N_Gq39kFDfEEUiU?usp=sharing
